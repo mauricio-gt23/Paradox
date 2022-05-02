1fa823d55402dbb5da54bdb6a40bfe35b21b707c
Carolina Milagros Villegas Celis <carolina.villegas@alarcongroup.us>
2022-05-01 21:07:44 -0500

    add: register employeer and postulant activity

--- app/src/main/java/com/example/paradox/Register.kt
+++ app/src/main/java/com/example/paradox/Register.kt
@@ -1,11 +1,28 @@
 package com.example.paradox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val btRegisterEmployeerActivity = findViewById<Button>(R.id.btRegisterEmployeer)
+        val btRegisterPostulantActivity = findViewById<Button>(R.id.btRegisterPostulant)
+
+        btRegisterEmployeerActivity.setOnClickListener {
+
+            val intent = Intent(this, RegisterEmployeerActivity::class.java)
+            startActivity(intent)
+        }
+
+        btRegisterPostulantActivity.setOnClickListener {
+
+            val intent = Intent(this, RegisterPostulantActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
