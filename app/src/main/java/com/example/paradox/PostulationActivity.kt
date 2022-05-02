82c8037b0004554f70d4ba37f9d25bc7d21f1cc4
Carolina Milagros Villegas Celis <carolina.villegas@alarcongroup.us>
2022-05-01 21:55:25 -0500

    add: login activity

---
+++ app/src/main/java/com/example/paradox/PostulationActivity.kt
@@ -0,0 +1,19 @@
+package com.example.paradox
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class PostulationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_postulation)
+
+
+
+    }
+
+
+}
--- app/src/main/java/com/example/paradox/RegisterPostulantActivity.kt
+++ app/src/main/java/com/example/paradox/RegisterPostulantActivity.kt
@@ -1,11 +1,21 @@
 package com.example.paradox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterPostulantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_postulant)
+        val etPerfilPostulante = findViewById<Button>(R.id.btnContinuarPostulante)
+
+
+        etPerfilPostulante.setOnClickListener {
+
+            val intent = Intent(this, PerfilPostulanteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
