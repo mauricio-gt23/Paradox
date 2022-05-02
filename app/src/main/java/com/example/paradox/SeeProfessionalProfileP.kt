b76a59e30e8e3ca4994e198d1a5dc08e96db7f64
BrigitteMendez1302 <U201914955@upc.edu.pe>
2022-05-01 23:40:57 -0500

    add: files created

---
+++ app/src/main/java/com/example/paradox/SeeProfessionalProfileP.kt
@@ -0,0 +1,11 @@
+package com.example.paradox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeeProfessionalProfileP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_see_professional_profile_p)
+    }
+}
