ddc49a091c797af05be7fa4dfc1f52a5555aa57a
brigittemendez <brigitte.mendez@alarcongroup.us>
2022-05-03 08:04:22 -0700

    add: horizontally and vertically aligned

--- app/src/main/java/com/example/paradox/SeeProfessionalProfileP.kt
+++ app/src/main/java/com/example/paradox/SeeProfessionalProfileP.kt
@@ -1,11 +1,20 @@
 package com.example.paradox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SeeProfessionalProfileP : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_see_professional_profile_p)
+
+//        val buttonEditProfessional = findViewById<Button>(R.id.btGoToEditProfessional)
+//        buttonEditProfessional.setOnClickListener {
+//            val intent = Intent(this, EditProfessionalProfileP::class.java)
+//            startActivity(intent)
+//        }
+
     }
 }
--- app/src/main/java/com/example/paradox/SeeProfilePostulant.kt
+++ app/src/main/java/com/example/paradox/SeeProfilePostulant.kt
@@ -1,11 +1,20 @@
 package com.example.paradox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SeeProfilePostulant : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_see_profile_postulant)
+
+//        val buttonEdit = findViewById<Button>(R.id.btEditPostulantProfile)
+//        buttonEdit.setOnClickListener {
+//            val intent = Intent(this, EditProfilePostulant::class.java)
+//            startActivity(intent)
+//        }
     }
+
 }
