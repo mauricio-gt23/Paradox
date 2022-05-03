2354ec6242dba57d71ceaffa9277aad05332a12f
brigittemendez <brigitte.mendez@alarcongroup.us>
2022-05-03 08:29:18 -0700

    add: working with inten

--- app/src/main/java/com/example/paradox/SeeProfessionalProfileP.kt
+++ app/src/main/java/com/example/paradox/SeeProfessionalProfileP.kt
@@ -3,18 +3,18 @@ package com.example.paradox
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class SeeProfessionalProfileP : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_see_professional_profile_p)
 
-//        val buttonEditProfessional = findViewById<Button>(R.id.btGoToEditProfessional)
-//        buttonEditProfessional.setOnClickListener {
-//            val intent = Intent(this, EditProfessionalProfileP::class.java)
-//            startActivity(intent)
-//        }
+        val buttonEditProfessional = findViewById<FloatingActionButton>(R.id.btGoToEditProfessional)
+        buttonEditProfessional.setOnClickListener {
+            val intent = Intent(this, EditProfessionalProfileP::class.java)
+            startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/com/example/paradox/SeeProfilePostulant.kt
+++ app/src/main/java/com/example/paradox/SeeProfilePostulant.kt
@@ -3,18 +3,18 @@ package com.example.paradox
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class SeeProfilePostulant : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_see_profile_postulant)
 
-//        val buttonEdit = findViewById<Button>(R.id.btEditPostulantProfile)
-//        buttonEdit.setOnClickListener {
-//            val intent = Intent(this, EditProfilePostulant::class.java)
-//            startActivity(intent)
-//        }
+        val buttonEdit = findViewById<FloatingActionButton>(R.id.btEditPostulantProfile)
+        buttonEdit.setOnClickListener {
+            val intent = Intent(this, EditProfilePostulant::class.java)
+            startActivity(intent)
+        }
     }
 
 }
