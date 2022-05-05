ded48d1b28b4cc75c565ef26b3031729e6c6c5a8
Carolina Milagros Villegas Celis <carolina.villegas@alarcongroup.us>
2022-05-05 18:00:02 -0500

    add: perfil postulant

--- app/src/main/java/com/example/paradox/PerfilPostulanteActivity.kt
+++ app/src/main/java/com/example/paradox/PerfilPostulanteActivity.kt
@@ -2,12 +2,47 @@ package com.example.paradox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class PerfilPostulanteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil_postulante)
+        val spinner_grado_estudios: Spinner = findViewById(R.id.spGradoDeEstudios)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.grado_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner_grado_estudios.adapter = adapter
+        }
 
+        val spinner_habilidades: Spinner = findViewById(R.id.spHabilidades)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.habilidades_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner_habilidades.adapter = adapter
+        }
+        val spinner_Idiomas: Spinner = findViewById(R.id.spIdiomas)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.idiomas_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner_Idiomas.adapter = adapter
+        }
 
     }
 }
