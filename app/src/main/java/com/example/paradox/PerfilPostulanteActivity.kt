a00443d104ed21593767f26cf2f6de68221fff04
Carolina Milagros Villegas Celis <carolina.villegas@alarcongroup.us>
2022-05-05 17:04:42 -0500

    add: activity perfil del postulante

--- app/src/main/java/com/example/paradox/PerfilPostulanteActivity.kt
+++ app/src/main/java/com/example/paradox/PerfilPostulanteActivity.kt
@@ -7,5 +7,7 @@ class PerfilPostulanteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil_postulante)
+
+
     }
 }
