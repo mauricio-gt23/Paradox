accf4eed91a3160e736b3266a28a54bd088dc319
brigittemendez <brigitte.mendez@alarcongroup.us>
2022-05-05 15:18:22 -0700

    add: dropdown

--- app/src/main/java/com/example/paradox/EditProfessionalProfileP.kt
+++ app/src/main/java/com/example/paradox/EditProfessionalProfileP.kt
@@ -2,10 +2,23 @@ package com.example.paradox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class EditProfessionalProfileP : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_professional_profile_p)
+        val spinner: Spinner = findViewById(R.id.spStudyDegree)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.planets_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
     }
 }
