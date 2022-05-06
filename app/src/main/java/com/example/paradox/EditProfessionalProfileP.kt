7e20935b51173872dda431e1e811ce3ba9cf89b4
brigittemendez <brigitte.mendez@alarcongroup.us>
2022-05-06 10:26:23 -0700

    add: avatar show

--- app/src/main/java/com/example/paradox/EditProfessionalProfileP.kt
+++ app/src/main/java/com/example/paradox/EditProfessionalProfileP.kt
@@ -20,5 +20,17 @@ class EditProfessionalProfileP : AppCompatActivity() {
             // Apply the adapter to the spinner
             spinner.adapter = adapter
         }
+
+        val spinner2: Spinner = findViewById(R.id.spSkills)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.skills_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner2.adapter = adapter
+        }
     }
 }
