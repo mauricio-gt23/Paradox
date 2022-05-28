755fd332bc4a39108b2f5ae0ddc3e1e017f75801
BrigitteMendez1302 <U201914955@upc.edu.pe>
2022-05-28 04:03:00 -0700

    fix: skills array

--- app/src/main/java/com/example/paradox/controller/activities/EditProfessionalProfileP.kt
+++ app/src/main/java/com/example/paradox/controller/activities/EditProfessionalProfileP.kt
@@ -16,7 +16,6 @@ class EditProfessionalProfileP : AppCompatActivity() {
     var languages = listOf<Language>()
     var skills = listOf<Skill>()
     var existentSkills = listOf<Skill>()
-    private val languagesList = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,28 +28,21 @@ class EditProfessionalProfileP : AppCompatActivity() {
             R.array.planets_array,
             android.R.layout.simple_spinner_item
         ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
             spinner.adapter = adapter
         }
-
         val spinner2: Spinner = findViewById(R.id.spSkills)
         ArrayAdapter.createFromResource(
             this,
             R.array.skills_array,
             android.R.layout.simple_spinner_item
         ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
             spinner2.adapter = adapter
         }
-
         tvMultiSelect.setOnClickListener{
             configMultiSelectSkills()
         }
-
         if (intent.extras != null) {
             loadData()
         }
@@ -126,6 +118,7 @@ class EditProfessionalProfileP : AppCompatActivity() {
         val checkedColorsArray = BooleanArray(skills.size)
         val tvMultiSelect = findViewById<TextView>(R.id.tvMultiSelectB)
         val builder = AlertDialog.Builder(this@EditProfessionalProfileP)
+        val languagesList = mutableListOf<String>()
         // String array for alert dialog multi choice items
         for (i in skills.indices) {
             languagesList.add(skills[i].name)
@@ -136,22 +129,14 @@ class EditProfessionalProfileP : AppCompatActivity() {
             }
         }
         val languagesArray: Array<String> = languagesList.toTypedArray()
-        // Boolean array for initial selected items
-        // Convert the color array to list
         val colorsList = listOf(*languagesArray)
         builder.setTitle("Select your skills")
-        //set multi choice
         builder.setMultiChoiceItems(languagesArray, checkedColorsArray) { _, which, isChecked ->
-            // Update the current focused item's checked status
             checkedColorsArray[which] = isChecked
-            // Get the current focused item
             val currentItem = colorsList[which]
-            // Notify the current action
             Toast.makeText(applicationContext, "$currentItem $isChecked", Toast.LENGTH_SHORT).show()
         }
-        // Set the positive/yes button click listener
         builder.setPositiveButton("OK") { _, _ ->
-            // Do something when click positive button
             tvMultiSelect.hint = "Select your skills..."
             tvMultiSelect.text = ""
             for (i in checkedColorsArray.indices) {
